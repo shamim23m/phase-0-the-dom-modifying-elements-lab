c1433e4a9c2c2c1f8ce5e3fbf11b9dcf38a4d96c
shamim23m <shamimmohamud13@gmail.com>
2024-10-10 17:54:30 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const newHeader = 
+document.createElement("h1");
+newHeader.id='victory'
+newHeader.textContent = 'Jose is the champion ';
+document.body.append(newHeader);
